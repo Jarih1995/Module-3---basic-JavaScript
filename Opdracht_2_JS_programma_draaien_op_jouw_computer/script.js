b68c24026c4adfa4744540a2bfb7921ec75cbd2d
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-09-16 16:35:21 +0200

    Add files via upload

---
+++ Opdracht_2_JS_programma_draaien_op_jouw_computer/script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten")
+
+/*
+let firstSum = 1230941 * 1823794;
+console.log(firstSum);
+
+// Dit is een grote som
+
+let secondSum = 637263 / 54;
+console.log(secondSum);
+*/
