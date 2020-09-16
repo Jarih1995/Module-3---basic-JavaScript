9889a28ac0d27c3b521b76344b3e91c5dd6c8407
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-09-16 15:07:04 +0200

    Add files via upload

---
+++ Module 3/Opdracht 1 - Gebruik je eerste JavaScript/script.js
@@ -0,0 +1,32 @@
+console.log("Hello Winc Academy");
+
+let firstName = "Jari ", lastName = "Helsloot"; 
+console.log(firstName + lastName);
+
+let firstSum = '4 + 4';
+console.log(firstSum);
+
+let secondSum = 4 + 4;
+console.log(secondSum);
+
+let thirdSum = "4" + "4";
+console.log(thirdSum);
+
+let fourthSum = 4 - 40;
+console.log(fourthSum);
+
+let fifthSum = 40 * 4;
+console.log(fifthSum);
+
+let sixthSum = 4 / 8;
+console.log(sixthSum);
+
+let seventhSum = 4 % 40;
+console.log(seventhSum);
+
+
+let age = "25"
+typeof 25 === 'text'
+console.log(age);
+
+
