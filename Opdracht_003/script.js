4e5adf2356046136cd233f00b21e51c7af1ab61f
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-09-17 12:13:52 +0200

    Add files via upload

---
+++ Opdracht_003/script.js
@@ -0,0 +1,32 @@
+var isFemale = false;
+
+if (isFemale = true) {
+    console.log("Mag ik uw id-kaart zien?")
+}
+
+else {
+    console.log("Sorry, het is ladiesnight..")
+}
+
+var myAge = 18;
+
+if (myAge > 17){
+
+    console.log("Komt u binnen en neem een lekker drankje")
+}
+
+else {
+    console.log("Wilt u zo vriendelijk zijn om op de donderen!")
+}
+
+var driverStatus = "henk";
+
+if (driverStatus == "bob"){
+    console.log("U mag rijden")
+}
+
+else{
+    console.log("u mag niet meer rijden")
+}
+
+
