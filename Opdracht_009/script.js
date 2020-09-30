cb07290c0f6435b4b0b1b4a922ff4b9ac011f601
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-09-30 16:23:42 +0200

    // Opdracht: Iterators - oude en nieuwe stijl

---
+++ Opdracht_009/script.js
@@ -0,0 +1,31 @@
+// Opdracht: Iterators - oude en nieuwe stijl
+
+// Oude stijl: loops
+
+// /While loop:
+
+
+// const colors = ["yellow", "red", "orange"];
+// // let i = 0; 
+
+// while (i < colors.length){
+//     console.log(colors[i]);
+//     i++
+// }
+
+// // For loop:
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i])
+// }
+
+const colors = ["yellow", "red", "orange"];
+
+colors.forEach((color) => console.log(color));
+
+
+// 1. for loop = 2 2. while loop = 4
+// er is 1 regel nodig
+// het woord element + je hoeft er geen index aan te geven
+
+
